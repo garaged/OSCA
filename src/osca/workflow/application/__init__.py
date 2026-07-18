61d0aba7a3e1aa5e6b5818475e98165c872453f8
garaged <garaged@gmail.com>
2026-07-18 15:17:36 -0600

    Add compatible generic durable job foundation

--- src/osca/workflow/application/__init__.py
+++ src/osca/workflow/application/__init__.py
@@ -1,3 +1,4 @@
 from osca.workflow.application.handlers import WorkflowService
+from osca.workflow.application.jobs import JobRepository, JobService
 
-__all__ = ["WorkflowService"]
+__all__ = ["JobRepository", "JobService", "WorkflowService"]
