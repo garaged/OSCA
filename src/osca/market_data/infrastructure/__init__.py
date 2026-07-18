1eb8163845a6fdb57d4a97c2119658ee7f9694f5
garaged <garaged@gmail.com>
2026-07-18 14:58:17 -0600

    Add exact Parquet payload publication

--- src/osca/market_data/infrastructure/__init__.py
+++ src/osca/market_data/infrastructure/__init__.py
@@ -3,4 +3,19 @@ from osca.market_data.infrastructure.persistence import (
     SqliteManifestRepository,
 )
 
-__all__ = ["MarketDataBase", "SqliteManifestRepository"]
+__all__ = [
+    "DAILY_BAR_SCHEMA",
+    "ImmutablePayloadStore",
+    "MarketDataBase",
+    "SqliteManifestRepository",
+    "deserialize_daily_bars",
+    "payload_digest",
+    "serialize_daily_bars",
+]
+from osca.market_data.infrastructure.parquet import (
+    DAILY_BAR_SCHEMA,
+    ImmutablePayloadStore,
+    deserialize_daily_bars,
+    payload_digest,
+    serialize_daily_bars,
+)
