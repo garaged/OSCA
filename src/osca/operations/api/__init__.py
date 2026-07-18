acaff6cd34ba83a07e847d6e7fbced8bb55423d7
garaged <garaged@gmail.com>
2026-07-18 05:53:45 -0600

    Complete M1.3 src/osca/operations/api/__init__.py

--- src/osca/operations/api/__init__.py
+++ src/osca/operations/api/__init__.py
@@ -1,4 +1,10 @@
+from .audit import AuditOutcome, AuditRecord
 from .contracts import ComponentReadiness, HealthState, ReadinessSnapshot
 
-__all__ = ["ComponentReadiness", "HealthState", "ReadinessSnapshot"]
-
+__all__ = [
+    "AuditOutcome",
+    "AuditRecord",
+    "ComponentReadiness",
+    "HealthState",
+    "ReadinessSnapshot",
+]
