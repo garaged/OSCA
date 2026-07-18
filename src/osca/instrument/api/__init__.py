d9c8884b8f85941402a9b13abd22ee2fb7867052
garaged <garaged@gmail.com>
2026-07-18 12:11:20 -0600

    feat(m2): build governed instrument registry

---
+++ src/osca/instrument/api/__init__.py
@@ -0,0 +1,15 @@
+from osca.instrument.api.contracts import (
+    AssetClass,
+    InstrumentLifecycle,
+    InstrumentReference,
+    MappingVerification,
+    ProviderMapping,
+)
+
+__all__ = [
+    "AssetClass",
+    "InstrumentLifecycle",
+    "InstrumentReference",
+    "MappingVerification",
+    "ProviderMapping",
+]
