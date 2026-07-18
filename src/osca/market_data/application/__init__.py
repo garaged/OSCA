5908b1efeb3110a1666575422753637c6db23798
garaged <garaged@gmail.com>
2026-07-18 15:03:52 -0600

    Sort Market Data application exports

--- src/osca/market_data/application/__init__.py
+++ src/osca/market_data/application/__init__.py
@@ -2,17 +2,17 @@ from osca.market_data.application.cleanup import CleanupPlan, preview_cleanup
 from osca.market_data.application.dates import classify_dates, contiguous_missing_ranges
 from osca.market_data.application.inspection import StorageInspection, inspect_storage
 from osca.market_data.application.normalize import IncompleteObservationError, normalize_daily
-from osca.market_data.application.quality import validate_daily_series
 from osca.market_data.application.publication import (
     CanonicalPublicationIntent,
     CanonicalPublisher,
 )
+from osca.market_data.application.quality import validate_daily_series
 from osca.market_data.application.retrieval import resolve_retrieval
 
 __all__ = [
-    "CleanupPlan",
     "CanonicalPublicationIntent",
     "CanonicalPublisher",
+    "CleanupPlan",
     "IncompleteObservationError",
     "StorageInspection",
     "classify_dates",
