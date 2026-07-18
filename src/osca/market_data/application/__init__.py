de100a25e5c735604f6e5c4968b486def871b8c6
garaged <garaged@gmail.com>
2026-07-18 14:43:57 -0600

    Implement M2 retrieval policy contracts

--- src/osca/market_data/application/__init__.py
+++ src/osca/market_data/application/__init__.py
@@ -1,3 +1,12 @@
+from osca.market_data.application.cleanup import CleanupPlan, preview_cleanup
+from osca.market_data.application.dates import classify_dates, contiguous_missing_ranges
 from osca.market_data.application.normalize import IncompleteObservationError, normalize_daily
 
-__all__ = ["IncompleteObservationError", "normalize_daily"]
+__all__ = [
+    "CleanupPlan",
+    "IncompleteObservationError",
+    "classify_dates",
+    "contiguous_missing_ranges",
+    "normalize_daily",
+    "preview_cleanup",
+]
