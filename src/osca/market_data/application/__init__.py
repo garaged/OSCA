d24a5ed477ef74baa397de092ca64ccfc6e59c83
garaged <garaged@gmail.com>
2026-07-18 14:45:43 -0600

    Test M2 freshness and exact revision resolution

--- src/osca/market_data/application/__init__.py
+++ src/osca/market_data/application/__init__.py
@@ -1,6 +1,7 @@
 from osca.market_data.application.cleanup import CleanupPlan, preview_cleanup
 from osca.market_data.application.dates import classify_dates, contiguous_missing_ranges
 from osca.market_data.application.normalize import IncompleteObservationError, normalize_daily
+from osca.market_data.application.retrieval import resolve_retrieval
 
 __all__ = [
     "CleanupPlan",
@@ -9,4 +10,5 @@ __all__ = [
     "contiguous_missing_ranges",
     "normalize_daily",
     "preview_cleanup",
+    "resolve_retrieval",
 ]
