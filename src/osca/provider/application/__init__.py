2f3289f3568dbfc83ee4761e2dcc1858eba8fc90
garaged <garaged@gmail.com>
2026-07-18 15:23:23 -0600

    Implement explicit provider routing and visible fallback

--- src/osca/provider/application/__init__.py
+++ src/osca/provider/application/__init__.py
@@ -1,3 +1,4 @@
 from osca.provider.application.ports import DailyProviderAdapter
+from osca.provider.application.routing import DailyProviderRouter, RoutedProviderResult
 
-__all__ = ["DailyProviderAdapter"]
+__all__ = ["DailyProviderAdapter", "DailyProviderRouter", "RoutedProviderResult"]
