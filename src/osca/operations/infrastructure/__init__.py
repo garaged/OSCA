5bc9ad8f56b102603a9b1ca9035f29f5924cc689
garaged <garaged@gmail.com>
2026-07-18 05:53:46 -0600

    Complete M1.3 src/osca/operations/infrastructure/__init__.py

--- src/osca/operations/infrastructure/__init__.py
+++ src/osca/operations/infrastructure/__init__.py
@@ -1,5 +1,10 @@
 from .logging import configure_json_logging
+from .persistence import AuditBase, SqliteAuditRepository
 from .telemetry import configure_telemetry
 
-__all__ = ["configure_json_logging", "configure_telemetry"]
-
+__all__ = [
+    "AuditBase",
+    "SqliteAuditRepository",
+    "configure_json_logging",
+    "configure_telemetry",
+]
