3445effd48312947025ab15a9e33e9ce5560692a
garaged <garaged@gmail.com>
2026-07-18 14:43:56 -0600

    Implement M2 retrieval policy contracts

--- src/osca/market_data/api/__init__.py
+++ src/osca/market_data/api/__init__.py
@@ -2,7 +2,12 @@ from osca.market_data.api.contracts import (
     CanonicalDailyBar,
     DatasetLayer,
     DatasetManifest,
+    DateClassification,
+    DateFinding,
     ManifestState,
+    ResolutionState,
+    RetrievalRequest,
+    RetrievalResolution,
     canonical_fingerprint,
 )
 
@@ -10,6 +15,11 @@ __all__ = [
     "CanonicalDailyBar",
     "DatasetLayer",
     "DatasetManifest",
+    "DateClassification",
+    "DateFinding",
     "ManifestState",
+    "ResolutionState",
+    "RetrievalRequest",
+    "RetrievalResolution",
     "canonical_fingerprint",
 ]
