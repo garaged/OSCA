9bcea018f0e3cb0691694a0d66674f4f1f5d589d
garaged <garaged@gmail.com>
2026-07-18 10:04:04 -0600

    feat(recovery): add authorized recovery application flow

--- src/osca/recovery/api/__init__.py
+++ src/osca/recovery/api/__init__.py
@@ -1,19 +1,27 @@
 from osca.recovery.api.contracts import (
     BackupManifest,
     BackupRecord,
+    CreateBackup,
+    ExecuteRestore,
     ManifestEntry,
+    PreviewRestore,
     RecoveryAvailability,
     RestorePlan,
     RestoreRecord,
     RestoreValidation,
+    VerifyBackup,
 )
 
 __all__ = [
     "BackupManifest",
     "BackupRecord",
+    "CreateBackup",
+    "ExecuteRestore",
     "ManifestEntry",
+    "PreviewRestore",
     "RecoveryAvailability",
     "RestorePlan",
     "RestoreRecord",
     "RestoreValidation",
+    "VerifyBackup",
 ]
