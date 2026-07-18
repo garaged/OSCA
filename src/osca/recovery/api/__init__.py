4d2ded5b6561325d26e3ed981c63f3922b5d530f
garaged <garaged@gmail.com>
2026-07-18 09:50:04 -0600

    feat(recovery): add versioned recovery contracts

---
+++ src/osca/recovery/api/__init__.py
@@ -0,0 +1,19 @@
+from osca.recovery.api.contracts import (
+    BackupManifest,
+    BackupRecord,
+    ManifestEntry,
+    RecoveryAvailability,
+    RestorePlan,
+    RestoreRecord,
+    RestoreValidation,
+)
+
+__all__ = [
+    "BackupManifest",
+    "BackupRecord",
+    "ManifestEntry",
+    "RecoveryAvailability",
+    "RestorePlan",
+    "RestoreRecord",
+    "RestoreValidation",
+]
