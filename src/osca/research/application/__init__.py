db769013f37a08c2ff491506a03f6cd4abe54fcd
garaged <garaged@gmail.com>
2026-07-24 19:14:55 -0600

    Sort research application exports

--- src/osca/research/application/__init__.py
+++ src/osca/research/application/__init__.py
@@ -2,14 +2,14 @@ from osca.research.application.services import (
     GraphExecutionPlan,
     assemble_evidence_report,
     plan_analysis_graph,
-    promote_ad_hoc_workspace,
     project_timeline,
+    promote_ad_hoc_workspace,
 )
 
 __all__ = [
     "GraphExecutionPlan",
     "assemble_evidence_report",
     "plan_analysis_graph",
-    "promote_ad_hoc_workspace",
     "project_timeline",
+    "promote_ad_hoc_workspace",
 ]
