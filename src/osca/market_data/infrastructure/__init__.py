47d38bc73568a17c69ee5eae8b48c00d30b95535
garaged <garaged@gmail.com>
2026-07-24 18:26:19 -0600

    Export OHLCV Parquet codec

--- src/osca/market_data/infrastructure/__init__.py
+++ src/osca/market_data/infrastructure/__init__.py
@@ -1,10 +1,14 @@
 from osca.market_data.infrastructure.parquet import (
     DAILY_BAR_SCHEMA,
+    OHLCV_BAR_SCHEMA,
     ImmutablePayloadStore,
     PyArrowCanonicalCodec,
+    PyArrowOhlcvCodec,
     deserialize_daily_bars,
+    deserialize_ohlcv_bars,
     payload_digest,
     serialize_daily_bars,
+    serialize_ohlcv_bars,
 )
 from osca.market_data.infrastructure.persistence import (
     MarketDataBase,
@@ -13,11 +17,15 @@ from osca.market_data.infrastructure.persistence import (
 
 __all__ = [
     "DAILY_BAR_SCHEMA",
+    "OHLCV_BAR_SCHEMA",
     "ImmutablePayloadStore",
     "MarketDataBase",
     "PyArrowCanonicalCodec",
+    "PyArrowOhlcvCodec",
     "SqliteManifestRepository",
     "deserialize_daily_bars",
+    "deserialize_ohlcv_bars",
     "payload_digest",
     "serialize_daily_bars",
+    "serialize_ohlcv_bars",
 ]
