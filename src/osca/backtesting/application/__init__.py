7b38273dce2c5553a14abd5121e17de33369df36
garaged <garaged@gmail.com>
2026-07-25 04:05:02 -0600

    Add M6 backtesting contracts and tests

---
+++ src/osca/backtesting/application/__init__.py
@@ -0,0 +1,9 @@
+from osca.backtesting.application.services import (
+    plan_backtest_execution,
+    validate_backtest_request,
+)
+
+__all__ = [
+    "plan_backtest_execution",
+    "validate_backtest_request",
+]
