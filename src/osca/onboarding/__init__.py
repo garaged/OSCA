2e826d88bffd9c396ee2ad7d4b36add02e9438e0
garaged <garaged@gmail.com>
2026-07-31 12:58:30 -0600

    Expose onboarding API

---
+++ src/osca/onboarding/__init__.py
@@ -0,0 +1,9 @@
+from osca.onboarding.contracts import OnboardingCheck, OnboardingReport, OnboardingStatus
+from osca.onboarding.services import inspect_first_run
+
+__all__ = [
+    "OnboardingCheck",
+    "OnboardingReport",
+    "OnboardingStatus",
+    "inspect_first_run",
+]
